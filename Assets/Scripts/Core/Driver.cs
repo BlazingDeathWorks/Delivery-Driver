e9cd08cf3fe5fea31e5bbefe0a78bccad4cd79f0
FutureInspireGames <shunsei.chan@gmail.com>
2021-11-12 16:23:58 -0800

    Driver Class

--- Assets/Scripts/Core/Driver.cs
+++ Assets/Scripts/Core/Driver.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Driver : MonoBehaviour
+internal class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] [Tooltip("The gameobject will rotate x degrees per second")] private float steerSpeed = 360;
+    [SerializeField] [Tooltip("The gameobject will move forward x units per second")] private float moveSpeed = 1;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmountX = Input.GetAxis("Horizontal");
+        float steerAmountY = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, steerAmountY * Time.deltaTime * moveSpeed));
+        transform.Rotate(new Vector3(0, 0, -steerAmountX * Time.deltaTime * steerSpeed));
     }
 }
